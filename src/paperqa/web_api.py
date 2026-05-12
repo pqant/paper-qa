608544e0838beaa80c16a534b6c3d19fb6dd1287
Eralp Erat <eralperatt@gmail.com>
2026-05-12 11:38:08 +0200

    chore: Expand CORS origins to include common Next.js dev ports
    Ports 3001-3003 added for when port 3000 is occupied.

--- src/paperqa/web_api.py
+++ src/paperqa/web_api.py
@@ -31,7 +31,7 @@ app = FastAPI(
 # CORS middleware for web UI
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:8080"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003", "http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
